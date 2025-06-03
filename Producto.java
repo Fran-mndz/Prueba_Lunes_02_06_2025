ff3f8d6db0b542e1a9ce2d9ea8858e56fedbb107
Fran-mndz <shiro54828@gmail.com>
2025-06-02 21:53:05 -0600

    Primera Clase Productos y sus constructores

---
+++ Producto.java
@@ -0,0 +1,58 @@
+public class Producto {
+    private int id;
+    private String nombre;
+    private String marca;
+    private double precio;
+
+    // Constructor
+    public Producto(int id, String nombre, String marca, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    // Getters y Setters (Encapsulamiento)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // Método para mostrar los datos del producto
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
